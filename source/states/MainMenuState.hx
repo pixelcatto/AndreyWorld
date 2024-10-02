3047611956332e29282290c6d3380c2e146afbb0
pixelcatto <musicallycatto@gmail.com>
2024-10-02 18:20:57 +0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -1,1 +1,17 @@
+package states;
 
+import flixel.FlxState;
+
+class MainMenuState extends FlxState
+{
+  FlxG.mouse.visible = true;
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
