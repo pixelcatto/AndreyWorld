bd745579017f21a4bb6ffaf36034d05bf211cf5b
pixelcatto <musicallycatto@gmail.com>
2024-10-02 18:27:21 +0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -1,10 +1,11 @@
 package states;
 
 import flixel.FlxState;
+import flixel.FlxG;
 
 class MainMenuState extends FlxState
 {
-  FlxG.mouse.visible = true;
+        FlxG.mouse.visible = true;
 	override public function create()
 	{
 		super.create();
